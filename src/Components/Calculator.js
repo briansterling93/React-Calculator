8d62047dec5dce3029228c5e6190bba2428cb4a6
briansterling93 <41628472+briansterling93@users.noreply.github.com>
2019-08-29 09:06:56 -0600

    styling changes

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -4,26 +4,35 @@ class Calculator extends React.Component {
     render() {
         return (
 <div id='main'>
-  <div id='result'></div>
-    <div id='calculator'>
+  <div id='result'>
+    <div id='inputContainer'>
+    <div id='userInput'>
+          Hello
+    </div>
+      <div id='userResult'>
+          this is
+      </div>
+    </div>
+  </div>
+     <div id='calculator'>
       <div id='btns'>
-        <input class='btn' type='button' id='acItem' value='AC'/>
-        <input class='btn' type='button' value='.'/>
-        <input class='btn' type='button' value='/'/>
-        <input class='btn' type='button' value='7'/>
-        <input class='btn' type='button' value='8'/>
-        <input class='btn' type='button' value='9'/>
-        <input class='btn' type='button' value='x'/>
-        <input class='btn' type='button' value='4'/>
-        <input class='btn' type='button' value='5'/>
-        <input class='btn' type='button' value='6'/>
-        <input class='btn' type='button' value='-'/>
-        <input class='btn' type='button' value='1'/>
-        <input class='btn' type='button' value='2'/>
-        <input class='btn' type='button' value='3'/>
-        <input class='btn' type='button'  value='+'/>
-        <input class='btn' type='button' id='zeroBtn' value='0'/>
-        <input class='btn' type='button' id='equalBtn' value='='/>
+        <input className='btn' type='button' id='acItem' value='AC'/>
+        <input className='btn' type='button' id='actionBtn' value='.'/>
+        <input className='btn' type='button' id='actionBtn' value='/'/>
+        <input className='btn' type='button' value='7'/>
+        <input className='btn' type='button' value='8'/>
+        <input className='btn' type='button' value='9'/>
+        <input className='btn' type='button' id='actionBtn' value='x'/>
+        <input className='btn' type='button' value='4'/>
+        <input className='btn' type='button' value='5'/>
+        <input className='btn' type='button' value='6'/>
+        <input className='btn' type='button' id='actionBtn' value='-'/>
+        <input className='btn' type='button' value='1'/>
+        <input className='btn' type='button' value='2'/>
+        <input className='btn' type='button' value='3'/>
+        <input className='btn' type='button' id='actionBtn'  value='+'/>
+        <input className='btn' type='button' id='zeroBtn' value='0'/>
+        <input className='btn' type='button' id='actionBtn' value='='/>
       </div>
     </div>
 </div>
